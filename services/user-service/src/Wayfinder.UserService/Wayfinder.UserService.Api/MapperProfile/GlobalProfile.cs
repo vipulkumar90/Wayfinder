b85e237bea0e786dfe1c876a939e1cfeb7618ed7
Vipul Kumar <66874875+vipulkumar90@users.noreply.github.com>
2025-09-27 21:40:58 +0530

    1. Initial CRUD operation for User Service 2. Integration with SQL Server

---
+++ services/user-service/src/Wayfinder.UserService/Wayfinder.UserService.Api/MapperProfile/GlobalProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Google.Protobuf.WellKnownTypes;
+
+namespace Wayfinder.UserService.Api.MapperProfile
+{
+    public class GlobalProfile : Profile
+    {
+        public GlobalProfile()
+        {
+            CreateMap<DateTime, Timestamp>()
+                .ConvertUsing(src => Timestamp.FromDateTime(src.ToUniversalTime()));
+            CreateMap<Timestamp, DateTime>()
+                .ConvertUsing(src => src.ToDateTime());
+        }
+    }
+}
---
+++ services/user-service/src/Wayfinder.UserService/Wayfinder.UserService.Api/gRPC/UserApi.cs
@@ -0,0 +1,145 @@
+﻿using AutoMapper;
+using Grpc.Core;
+using User.V1;
+using Wayfinder.UserService.Api.Entity;
+using Wayfinder.UserService.Api.Repository;
+
+namespace Wayfinder.UserService.Api.gRPC
+{
+    public class UserApi : User.V1.UserService.UserServiceBase
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public UserApi(IUserRepository userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        public override async Task<CreateUserResponse> CreateUser(CreateUserRequest request, ServerCallContext context)
+        {
+            try
+            {
+                var createdUser = await _userRepository.AddUserAsync(_mapper.Map<UserEntity>(request.User));
+                var test = _mapper.Map<User.V1.User>(createdUser);
+                return new CreateUserResponse
+                {
+                    User = test
+                };
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public override async Task<GetAllUsersResponse> GetAllUsers(GetAllUsersRequest request, ServerCallContext context)
+        {
+            try
+            {
+                var users = await _userRepository.GetAllUsersAsync();
+                return new GetAllUsersResponse
+                {
+                    Users = { _mapper.Map<IEnumerable<User.V1.User>>(users) }
+                };
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public override async Task<DeleteUserResponse> DeleteUser(DeleteUserRequest request, ServerCallContext context)
+        {
+            try
+            {
+                return new DeleteUserResponse
+                {
+                    Success = await _userRepository.DeleteUserAsync(Guid.Parse(request.Id))
+                };
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public override async Task<GetAllUsersResponse> GetAllActiveUsers(GetAllUsersRequest request, ServerCallContext context)
+        {
+            try
+            {
+                return new GetAllUsersResponse
+                {
+                    Users = { _mapper.Map<List<User.V1.User>>(await _userRepository.GetAllActiveUsersAsync()) }
+                };
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public override async Task<GetUserResponse> GetUserByEmail(GetUserByEmailRequest request, ServerCallContext context)
+        {
+            try
+            {
+                return new GetUserResponse
+                {
+                    User = _mapper.Map<User.V1.User>(await _userRepository.GetUserAsync(u => u.Email == request.Email))
+                };
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public override async Task<GetUserResponse> GetUserById(GetUserByIdRequest request, ServerCallContext context)
+        {
+            try
+            {
+                return new GetUserResponse
+                {
+                    User = _mapper.Map<User.V1.User>(await _userRepository.GetUserAsync(u => u.Id == Guid.Parse(request.Id)))
+                };
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public override async Task<GetUserResponse> GetUserByUsername(GetUserByUsernameRequest request, ServerCallContext context)
+        {
+            try
+            {
+                return new GetUserResponse
+                {
+                    User = _mapper.Map<User.V1.User>(await _userRepository.GetUserAsync(u => u.Username == request.Username))
+                };
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public override async Task<UpdateUserResponse> UpdateUser(UpdateUserRequest request, ServerCallContext context)
+        {
+            try
+            {
+                return new UpdateUserResponse
+                {
+                    User = _mapper.Map<User.V1.User>(await _userRepository.UpdateUserAsync(_mapper.Map<UserEntity>(request.User)))
+                };
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
